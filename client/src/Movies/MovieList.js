330f8b49f124d64f1d6a529286e2eef6fff0ad90
Violeta Dimov <judyvy@gmail.com>
2020-08-18 12:16:59 -0400

    initial commit

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 const MovieList = props => {
   return (
@@ -14,7 +15,7 @@ function MovieDetails({ movie }) {
   const { title, director, metascore } = movie;
   return (
     <div className="movie-card">
-      <h2>{title}</h2>
+      <Link><h2>{title}</h2></Link>
       <div className="movie-director">
         Director: <em>{director}</em>
       </div>
